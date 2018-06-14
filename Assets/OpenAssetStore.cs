888ef274c7eecec7d6c58d058be1a1944eb70fd7
Yilmaz Kiymaz <ylmzkymz@gmail.com>
2018-06-14 22:45:08 +0200

    Added Tools slide

---
+++ Assets/OpenAssetStore.cs
@@ -0,0 +1,13 @@
+﻿using System.Reflection;
+using UnityEngine;
+
+public class OpenAssetStore : MonoBehaviour {
+
+	public void OnButtonClicked()
+	{
+		#if UNITY_EDITOR
+		var type = Assembly.GetAssembly(typeof(UnityEditor.EditorWindow)).GetType("UnityEditor.AssetStoreWindow");
+		UnityEditor.EditorWindow.GetWindow(type);
+		#endif
+	}
+}
