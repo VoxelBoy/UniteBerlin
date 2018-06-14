8bd1c973404d8243b73e3e579f60cd309c0fcd38
Yilmaz Kiymaz <ylmzkymz@gmail.com>
2018-06-15 01:59:35 +0200

    MenuItem and shortcut examples

---
+++ Assets/Scripts/Editor/LightingMenuItem.cs
@@ -0,0 +1,10 @@
+﻿using UnityEditor;
+
+public class LightingMenuItem {
+
+	[MenuItem("Window/Custom/LightingMenuItem %#l")]
+	public static void Show()
+	{
+		EditorApplication.ExecuteMenuItem("Window/Lighting/Settings");
+	}
+}
---
+++ Assets/Scripts/Editor/MakeParentCustom.cs
@@ -0,0 +1,26 @@
+﻿using System.Linq;
+using UnityEngine;
+using UnityEditor;
+
+public class MakeParentCustom {
+
+    [MenuItem("Window/Custom/Make Parent %#&p")]
+    public static void Show()
+    {
+        var selection = Selection.GetFiltered<GameObject>(SelectionMode.Editable | SelectionMode.TopLevel).ToList();
+        if (selection.Count == 0)
+        {
+            return;
+        }
+
+        var position = Vector3.zero;
+        selection.ForEach(x => position += x.transform.position);
+        position /= selection.Count;
+
+        var parent = new GameObject("parent");
+        Undo.RegisterCreatedObjectUndo (parent, "Created parent");
+        
+        parent.transform.position = position;
+        selection.ForEach(x => Undo.SetTransformParent(x.transform, parent.transform, "Set new parent"));
+    }
+}
