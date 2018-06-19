4a3a529bd1bc1f6f906f00417ae29d4d9a26b5ac
Yilmaz Kiymaz <yilmaz.kiymaz@innogames.com>
2018-06-19 12:51:00 +0200

    Added warning slide Added link to open Package Manager window

---
+++ Assets/Scripts/OpenPackageManager.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+public class OpenPackageManager : MonoBehaviour
+{
+    public void OnButtonClicked()
+    {
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.ExecuteMenuItem("Window/Package Manager");
+#endif
+    }
+}
