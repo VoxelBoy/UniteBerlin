fc9a5fc580b1aff3ad1eadebc3db4849ec2124b2
Yilmaz Kiymaz <yilmaz.kiymaz@innogames.com>
2018-06-20 09:45:09 +0200

    More fixes

---
+++ Assets/Scripts/Editor/SlideContextMenu.cs
@@ -0,0 +1,13 @@
+﻿using System.Linq;
+using TMPro;
+using UnityEditor;
+
+public class SlideContextMenu
+{
+    [MenuItem("CONTEXT/Slide/Gather text elements")]
+    private static void GatherTextElements(MenuCommand menuCommand)
+    {
+        var slide = menuCommand.context as Slide;
+        slide.Elements = slide.GetComponentsInChildren<TextMeshProUGUI>().ToList().ConvertAll(x => x.gameObject);
+    }
+}
